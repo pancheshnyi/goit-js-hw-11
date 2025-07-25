2a3383965d438e648f382d79a9e4d94a46c7541e
Oleg_Pancheshniy <oleg.pancheshniy@gmail.com>
2025-07-25 15:51:54 +0300

    fix fosition iziToast

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { getImagesByQuery } from './js/pixabay-api';
+import { getImagesByQuery } from './js/pixabay-api.js';
 import {
   createGallery,
   clearGallery,
   showLoader,
   hideLoader,
-} from './js/render-functions';
+} from './js/render-functions.js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
